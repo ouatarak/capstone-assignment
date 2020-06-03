8bb73c561be37a5dcd50c3183432f76c03cc4d47
ouatarak@yahoo.fr <hellokounandi.codes>
2020-06-03 15:06:46 -0600

    first commit

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
