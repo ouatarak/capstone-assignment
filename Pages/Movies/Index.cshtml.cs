9cc0ffd8395b00617afdb7e07d89defd9d08cd0f
ouatarak@yahoo.fr <hellokounandi.codes>
2020-06-04 01:12:19 -0600

    added filter by title

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,9 @@ using RazorPagesMovie.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using RazorPagesMovie.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace RazorPagesMovie.Pages.Movies
 {
@@ -18,9 +21,23 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
