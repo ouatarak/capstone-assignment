df18c618e01d33de33e059f88568ee6323310af6
ouatarak@yahoo.fr <hellokounandi.codes>
2020-06-06 14:12:04 -0600

    added a Validation on Title and Genre properties

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,12 +6,19 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Genre { get; set; }
-        public decimal Price { get; set; }
+
+        public decimal Price { get; set; } 
         public string Rating { get; set; }
     }
 }
